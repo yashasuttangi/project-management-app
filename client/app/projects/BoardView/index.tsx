8c29eb7b0b0816d77e00527667480971d2e5ccfe
Yashas Uttangi <yashuttangi@gmail.com>
2025-11-27 11:24:42 -0500

    Adding board view and some sample images.

--- client/app/projects/BoardView/index.tsx
+++ client/app/projects/BoardView/index.tsx
@@ -1,105 +1,264 @@
-import { useGetTasksQuery, useUpdateTaskStatusMutation } from '@/state/api';
-import React from 'react'
-import { DndProvider, useDrop } from "react-dnd";
+import { useGetTasksQuery, useUpdateTaskStatusMutation } from "@/state/api";
+import React from "react";
+import { DndProvider, useDrop, useDrag } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Task as TaskType } from "@/state/api";
+import { EllipsisVertical, MessageSquare, MessageSquareMore, Plus } from "lucide-react";
+import { format } from "date-fns";
+import Image from "next/image";
 
-type  BoardProps = {
-    id: string;
-    setIsModalNewTaskOpen: (isOpen: boolean) => void;
+type BoardProps = {
+  id: string;
+  setIsModalNewTaskOpen: (isOpen: boolean) => void;
 };
 
 const taskStatus = ["To Do", "Work In Progress", "Under Review", "Completed"];
 
-const BoardView = ({ id, setIsModalNewTaskOpen}: BoardProps) => {
+const BoardView = ({ id, setIsModalNewTaskOpen }: BoardProps) => {
+  const {
+    data: tasks,
+    isLoading,
+    error,
+  } = useGetTasksQuery({ projectId: Number(id) });
+  const [updateTaskStatus] = useUpdateTaskStatusMutation();
 
-    const { data: tasks, isLoading, error } = useGetTasksQuery({ projectId: Number(id) });
-    const [updateTaskStatus] = useUpdateTaskStatusMutation();
+  const moveTask = (taskId: number, toStatus: string) => {
+    updateTaskStatus({
+      taskId,
+      status: toStatus,
+    });
+  };
 
-    const moveTask = (taskId: number, toStatus: string) => {
-        updateTaskStatus({
-            taskId, status: toStatus
-        });
-    };
+  if (isLoading) return <div> Loading... </div>;
 
-    if (isLoading) return <div> Loading... </div>;
+  if (error) return <div> An error occured while fetching tasks </div>;
 
-    if (error) return <div> An error occured while fetching tasks </div>;
-
-
-
-    return (
-        <DndProvider backend={HTML5Backend}>
-            <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4'>
-                {taskStatus.map((status) => (
-                    <TaskColumn
-                        key={status}
-                        status={status}
-                        tasks={tasks || []}
-                        moveTask={moveTask}
-                        setIsModalNewTaskOpen={setIsModalNewTaskOpen}
-                    />
-                ))}
-            </div>
-        </DndProvider>
-    )
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-4">
+        {taskStatus.map((status) => (
+          <TaskColumn
+            key={status}
+            status={status}
+            tasks={tasks || []}
+            moveTask={moveTask}
+            setIsModalNewTaskOpen={setIsModalNewTaskOpen}
+          />
+        ))}
+      </div>
+    </DndProvider>
+  );
 };
 
 type TaskColumnProps = {
-    status: string;
-    tasks: TaskType[];
-    moveTask: (taskId: number, toStatus: string) => void;
-    setIsModalNewTaskOpen: (isOpen: boolean) => void;
-}
+  status: string;
+  tasks: TaskType[];
+  moveTask: (taskId: number, toStatus: string) => void;
+  setIsModalNewTaskOpen: (isOpen: boolean) => void;
+};
 
 const TaskColumn = ({
-    status,
-    tasks,
-    moveTask,
-    setIsModalNewTaskOpen,
+  status,
+  tasks,
+  moveTask,
+  setIsModalNewTaskOpen,
 }: TaskColumnProps) => {
-    const [{ isOver }, drop] = useDrop(() => ({
-        accept: "task",
-        drop: (item: {id: number}) => moveTask(item.id, status),
-        collect: (monitor: any) => ({
-            isOver: !!monitor.isOver()
-        })
-    }));
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "task",
+    drop: (item: { id: number }) => moveTask(item.id, status),
+    collect: (monitor: any) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
 
-    const tasksCount = tasks.filter((task) => task.status === status).length;
+  const tasksCount = tasks.filter((task) => task.status === status).length;
 
-    const statusColor: any = {
-        "To Do": "#2563EB",
-        "Work In Progress": "#D97706",
-        "Under Review": "#000000",
-        "Completed": "#059669"
-    }
+  const statusColor: any = {
+    "To Do": "#2563EB",
+    "Work In Progress": "#D97706",
+    "Under Review": "#000000",
+    Completed: "#059669",
+  };
 
-    return (
+  return (
+    <div
+      ref={(instance) => {
+        drop(instance);
+      }}
+      className={`sl:py-4 rounded-lg py-2 xl:px-2 ${isOver ? "bg-blue-100 dark:bg-neutral-950" : ""}`}
+    >
+      <div className="mb-3 flex w-full">
         <div
-            ref={(instance) => {
-                drop(instance);
-            }}
-            className={`sl:py-4 rounded-lg py-2 xl:px-2 ${isOver ? "bg-blue-100 dark:bg-neutral-950" : ""}`}
-        >
-            <div className='mb-3 flex w-full'> 
-                <div className={`w-2 !bg-[${statusColor[status]}] rounded-s-lg`}
-                    style={{ backgroundColor: statusColor[status] }}
-                />
-                <div className='flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4 dark:bg-dark-secondary'>
-                    <h3 className='flex items-center text-lg font-semibold dark:text-white'>
-                        { status } { " " }
-                    </h3>
-                    <span
-                        className='ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary'
-                        style={{ width: "1.5rem", height: "1.5rem" }}
-                    >
-                        { tasksCount }
+          className={`w-2 !bg-[${statusColor[status]}] rounded-s-lg`}
+          style={{ backgroundColor: statusColor[status] }}
+        />
+        <div className="dark:bg-dark-secondary flex w-full items-center justify-between rounded-e-lg bg-white px-5 py-4">
+          <h3 className="flex items-center text-lg font-semibold dark:text-white">
+            {status}{" "}
+            <span
+              className="dark:bg-dark-tertiary ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none"
+              style={{ width: "1.5rem", height: "1.5rem" }}
+            >
+              {tasksCount}
+            </span>
+          </h3>
+          <div className="flex items-center gap-1">
+            <button className="flex h-6 w-5 cursor-pointer items-center justify-center dark:text-neutral-500">
+              <EllipsisVertical size={36} />
+            </button>
+            <button
+              className="dark:bg-dark-tertiary flex h-6 w-6 cursor-pointer items-center justify-center rounded bg-gray-200 dark:text-white"
+              onClick={() => setIsModalNewTaskOpen(true)}
+            >
+              <Plus size={16} />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {tasks
+        .filter((task) => task.status === status)
+        .map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+    </div>
+  );
+};
+
+type TaskProps = {
+  task: TaskType;
+};
+
+const Task = ({ task }: TaskProps) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "task",
+    item: { id: task.id },
+    collect: (monitor: any) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  const taskTagsSplit = task.tags ? task.tags.split(",") : [];
+
+  const formattedStartDate = task.startDate
+    ? format(new Date(task.startDate), "P")
+    : "";
+  const formattedDueDate = task.dueDate
+    ? format(new Date(task.dueDate), "P")
+    : "";
+
+  const numberOfComments = (task.comments && task.comments.length) || 0;
+
+  const PriorityTag = ({ priority }: { priority: TaskType["priority"] }) => (
+    <div
+      className={`rounded-full px-2 py-1 text-xs font-semibold ${
+        priority === "Urgent"
+          ? "bg-red-200 text-red-700"
+          : priority === "High"
+            ? "bg-yellow-200 text-yellow-700"
+            : priority === "Medium"
+              ? "bg-green-200 text-green-700"
+              : priority === "Low"
+                ? "bg-blue-200 text-blue-700"
+                : "bg-gray-200 text-gray-700"
+      }`}
+    >
+      {priority}
+    </div>
+  );
+
+  return (
+    <div
+        ref={(instance) => {
+            drag(instance)
+        }}
+        className={`mg-4 rounded-md bg-white shadow dark:bg-dark-secondary ${
+            isDragging ? "opacity-50" : "opacity-100"
+        }`}  
+    >
+        { task.attachments && task.attachments.length > 0 && (
+            <Image
+                src={`/${task.attachments[0].fileURL}`}
+                alt={task.attachments[0].fileName}
+                width={400}
+                height={200}
+                className="h-auto w-full rounded-t-md"
+            />
+        )}
+        <div className="p-4 md:p-6">
+            <div className="flex items-start justify-between">
+                <div className="flex flex-1 flex-wrap items-center gap-2">
+                    { task.priority && <PriorityTag priority={task.priority} /> }
+                    <div className="flex gap-2">
+                        { taskTagsSplit.map((tag) => (
+                            <div className="rounded-full bg-blue-100 px-2 py-1 text-xs"
+                            key={tag}>
+                                { " " }
+                                { tag }
+                            </div>    
+                        ))}
+                    </div>
+                </div>
+                <button className="flex h-6 w-4 shrink-0 items-center justify-center dark:text-neutral-500">
+                    <EllipsisVertical size={26} />
+                </button>
+            </div>
+
+            <div className="my-3 flex justify-between">
+                <h4 className="text-md font-bold dark:text-white">
+                    { task.title }
+                </h4>
+                { typeof task.points === "number" && (
+                    <div className="text-xs font-semibold dark:text-white">
+                        { task.points } pts
+                    </div>
+                )}
+            </div>
+
+            <div className="text-xs text-gray-500 dark:text-neutral-500">
+                { formattedStartDate && <span> { formattedStartDate } - </span> }
+                { formattedDueDate && <span> { formattedDueDate }</span> }
+            </div>
+            <p className="text-sm text-gray-600 dark:text-neutral-500">
+                { task.description }
+            </p>
+            <div className="mt-4 border-t border-gray-200 dark:border-stroke-dark" />
+
+            { /* Users */ }
+            <div className="mt-3 flex items-center justify-between">
+                <div className="flex -space-x-1.5 oerflow-hidden">
+                    { task.assignee && (
+                        <Image
+                            key={task.assignee.userId}
+                            src={`/${task.assignee.profilePictureUrl!}`}
+                            alt={task.assignee.username}
+                            width={30}
+                            height={30}
+                            className="h-8 w-8 rounded-full border-2 border-white object-cover dark:border-dark-secondary"
+                        /> 
+                    )}
+                    { task.author && (
+                        <Image
+                            key={task.author.userId}
+                            src={`/${task.author.profilePictureUrl!}`}
+                            alt={task.author.username}
+                            width={30}
+                            height={30}
+                            className="h-8 w-8 rounded-full border-2 border-white object-cover dark:border-dark-secondary"
+                        /> 
+                    )}
+                </div>
+                <div className="flex items-center text-gray-500 dark:text-neutral-500">
+                    <MessageSquareMore size={20} />
+                    <span className="ml-1 text-sm dark:text-neutral-400">
+                        { numberOfComments }
                     </span>
                 </div>
             </div>
         </div>
-    )
-}
+    </div>
+  )
+
+};
 
 export default BoardView;
